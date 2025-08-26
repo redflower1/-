483a7831a38050c0fa4220152b0645f217785340
redflower1 <144190195+redflower1@users.noreply.github.com>
2025-08-26 17:45:00 +0800

    Update build.gradle.kts

--- test002/app/build.gradle.kts
+++ test002/app/build.gradle.kts
@@ -59,6 +59,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    
+    implementation("com.google.android.material:material:1.11.0")
     
     // Game development dependencies
     implementation("androidx.compose.animation:animation:1.5.4")
